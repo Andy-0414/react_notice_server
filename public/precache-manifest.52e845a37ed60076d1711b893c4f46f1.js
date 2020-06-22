242759e3311744af3d7ec626e3396acf7945f1c1
Andy-0414 <pjh8667@naver.com>
2020-06-22 21:39:40 +0900

    Update front build

---
+++ public/precache-manifest.52e845a37ed60076d1711b893c4f46f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf1a979a3fb377192823c92d89f8add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d7e4357c0da9ef151d",
+    "url": "/static/css/main.1f0761ee.chunk.css"
+  },
+  {
+    "revision": "c37ffcbe29229b303808",
+    "url": "/static/js/2.287a312f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.287a312f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d7e4357c0da9ef151d",
+    "url": "/static/js/main.47ac77c6.chunk.js"
+  },
+  {
+    "revision": "709267cc6ef3b96f8f67",
+    "url": "/static/js/runtime-main.edcdb1b2.js"
+  }
+]);
